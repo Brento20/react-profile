c2f0d7f049a45e7d367d2ee5393e89fcd1f0f82c
Brenton Weaver <brenton.weaver@gmail.com>
2022-04-25 22:59:16 +1000

    added some more content

--- react-profile-brenton/src/components/pages/About.js
+++ react-profile-brenton/src/components/pages/About.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div class="col-sm"> 
+    <h3 id="about_me">about me</h3>
+        <p>
+            I’m Brenton, nice to meet ya! 
+            
+            Originally from a small country town, I made the move to Sydney and while at the time I was terrified, I’ve always known how to build a community around me. After working in different industries, I'm uprooting my professional career and just like I did moving to Sydney, I’m taking a sharp left into web development and out of my comfort zone.
+            
+            In my professional life I have had so many wonderful opportunities to mentor and develop people around me and it's that drive and passion that has motivated me to branch out, the truth of it is, the experience I've gained from leading teams and working collaboratively has made me a strong communicator able to achieve goals quickly, effectively and in a way that engages those around me and I want to do that in an environment that stimulates and challenges me.
+            <a href="./assets/Brenton-Weaver-Resume.pdf" download="Brenton Weaver Resume">Download my Resume</a>
+        </p>
     </div>
   );
 }
